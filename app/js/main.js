3ac8d8da001a1d68fb48a3ace2f3a364c1787ea3
Kramskoy-v <71666173+Kramskoy-v@users.noreply.github.com>
2022-03-21 12:29:26 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,57 @@
+// Отправка данных на сервер
+let modalWindow = document.querySelector('.modal');
+   let modalClose = document.querySelector('.modal__close');
+      modalClose.addEventListener('click', function() {
+      modalWindow.classList.remove('modal--active')});
+      
+function send(event, php){
+   event.preventDefault ? event.preventDefault() : event.returnValue = false;
+   let req = new XMLHttpRequest();
+   req.open('POST', php, true);
+   req.onload = function() {
+      if (req.status >= 200 && req.status < 400) {
+      json = JSON.parse(this.response); // ie 11
+          console.log(json);
+           
+          // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+          if (json.result == 'success') {
+             // Если сообщение отправлено
+              modalWindow.classList.add('modal--active');
+              form.reset();
+          } else {
+             // Если произошла ошибка
+             alert('Ошибка. Сообщение не отправлено');
+          }
+       // Если не удалось связаться с php файлом
+       } else {alert('Ошибка сервера. Номер: '+req.status);}}; 
+   
+   // Если не удалось отправить запрос. Стоит блок на хостинге
+   req.onerror = function() {alert('Ошибка отправки запроса');};
+   req.send(new FormData(event.target));
+   }
+
+//Hamburger menu
+let hamburger = document.querySelector('.hamburger');
+let mobileMenu = document.querySelector('.menu');
+//let linkClose = document.querySelector('.menu__list');
+
+hamburger.addEventListener('click', function() {
+hamburger.classList.toggle('hamburger--active') 
+& mobileMenu.classList.toggle('menu--active')
+});
+
+function closeMenu() {
+   mobileMenu.classList.remove('menu--active');
+}
+function closeHam() {
+   hamburger.classList.remove('hamburger--active');
+}
+
+linkClose.addEventListener('touchstart', function(event){
+   if (event.target.closest('.menu__link')){
+      closeMenu(), closeHam();
+   }
+})
+
+//INITIALIZE AOS
+AOS.init();
---
+++ app/send.php
@@ -0,0 +1,53 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['username'];
+$phone = $_POST['userphone'];
+
+// Формирование самого письма
+$title = "Заказ с сайта грузоперевозок";
+$body = "
+<h2>Новый заказ</h2>
+<b>Имя:</b> $name<br>
+<b>Номер телефона:</b> $phone<br><br>";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'kramskoy_va@mail.ru'; // Логин на почте
+    $mail->Password   = '****'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('kramskoy_va@mail.ru', 'Сайт грузоперевозок'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('samsonov_perevezet@mail.ru');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
